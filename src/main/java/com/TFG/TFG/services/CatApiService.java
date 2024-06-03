f9d43b370c988ba64d8c921f1a77f92d2ccc509d
unknown <noeliatinajero1996@gmail.com>
2024-06-03 15:04:56 +0200

    comentarios Javadoc

--- src/main/java/com/TFG/TFG/services/CatApiService.java
+++ src/main/java/com/TFG/TFG/services/CatApiService.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
-
+/**
+ * Servicio para interactuar con la API de The Cat API.
+ */
 @Service
 public class CatApiService {
 
@@ -13,10 +15,18 @@ public class CatApiService {
 
     private final WebClient webClient;
 
+    /**
+     * Constructor para inicializar el cliente WebClient con la URL base de The Cat API.
+     */
     public CatApiService() {
         this.webClient = WebClient.create("https://api.thecatapi.com/v1/images/");
     }
 
+    /**
+     * Obtiene una imagen aleatoria de un gato.
+     * 
+     * @return una imagen aleatoria de un gato
+     */
     public Object getImages() {
         return webClient.get()
                 .uri(uriBuilder -> uriBuilder.path("/search")
@@ -25,8 +35,14 @@ public class CatApiService {
                 .retrieve()
                 .bodyToMono(Object.class)
                 .block();
-    }  
+    }
 
+    /**
+     * Obtiene un número limitado de imágenes de gatos.
+     * 
+     * @param limit el número máximo de imágenes a obtener
+     * @return una lista de imágenes de gatos
+     */
     public Object getImagesLimit(int limit) {
         return webClient.get()
                 .uri(uriBuilder -> uriBuilder.path("/search")
@@ -37,6 +53,4 @@ public class CatApiService {
                 .bodyToMono(Object.class)
                 .block();
     }
-    
 }
-
